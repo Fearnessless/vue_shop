be81e1fde47059ab62cc384fcd930b19bb528abd
yung <1048919271@qq.com>
2020-08-18 10:12:08 +0800

    Login

--- src/router.js
+++ src/router.js
@@ -1,11 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './components/Login.vue'
+import Home from './components/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
-    { path: '/login', component: Login }
+    // 路由重定向
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    { path: '/home', component: Home }
   ]
 })
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 表示将要访问的路径
+  // from 表示从哪个路径跳转过来的
+  // next 是一个函数，表示放行
+  //      next() 放行    next('/login') 强制跳转
+
+  if (to.path === '/login') return next()
+  // 获取 token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  else next()
+})
+
+export default router
